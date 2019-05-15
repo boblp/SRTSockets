cfe0e02c4bc6e9dda4b3eb72f3f96a4b34a242b9
boblp <robertolozano0011@gmail.com>
2019-05-14 23:54:05 -0500

    msg test

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-var express = require("express");
-var app = express();
-var http = require("http");
-var server = http.createServer(app);
+const express = require("express");
+const app = express();
+const http = require("http");
+const jwt = require('jsonwebtoken');
+const server = http.createServer(app);
 
 server.listen(process.env.PORT || 8080, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
@@ -10,8 +11,11 @@ server.listen(process.env.PORT || 8080, function(){
 const io = require('socket.io')(server);
 
 io.on('connection', socket => {
-	io.emit('respuesta_server', "holis");
-	socket.on('prueba_socketss', (var1, var2) => {
-		io.emit('respuesta_server', "holis");
+	socket.on('send_msg_from_user', (token, section, msg) => {
+		jwt.verify(request.query.auth, 'secret', function(err, decoded) {
+	    	if (err){ resolve('Invalid Code') } else {
+	    		io.emit('send_msg', decoded.name, section, msg);
+	    	}
+	    });
 	});
 });
